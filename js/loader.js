5c2daa590306c60f3a86d588f154a908ae81bfdc
seeward <seeward@gmail.com>
2014-12-14 19:16:24 +0200

    new

--- js/loader.js
+++ js/loader.js
@@ -1,33 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var bList = {};
+    var bTable = "";
 
-var bList = {};
-var bTable = "";
-	$.ajax({
-  url: "http://seeward.com/books_app.php",
-  cache: true,
- dataType : "json",
-}).done(function( html ) {
-	$("#loader").hide();
-
-renderTable(html);
-    
-});
-
-renderTable = function(data){
-	bTable = "<tr><th>Title</th><th>Author</th><th>Date</th></tr>"
-	$.each(data, function(i,obj){
-		bTable = bTable + "<tr><td>"+obj.NAME+"</td><td>"+obj.AUTHOR+"</td><td>"+obj.DATE+"</td></tr>";
-	});
-
-	$("#bookTable").html(bTable);
-};
-	
+    renderTable = function(data) {
 
 
-$("#bookTable").searcher({
-    inputSelector: "#searchTerm"
-    // itemSelector (tbody > tr) and textSelector (td) already have proper default values
-});
+        $.each(data, function(i, obj) {
+            if (obj.NAME != "") {
+                //console.log(JSON.stringify(obj))
+                bTable = bTable + "<tr><td>" + obj.NAME + "</td><td>" + obj.AUTHOR + "</td><td>" + obj.DATE + "</td></tr>";
+            }
+
+        });
+        $("#loader").hide()
+        window.localStorage.setItem("tabler", bTable);
+bTableHead = "<tr><th>Title</th><th>Author</th><th>Date</th></tr>"
+$("#bookTable").html(bTableHead);
+        $("#bookTable").append(bTable);
+
+    };
+
+
+    init = function() {
+        gather();
+    }
+
+
+    $(".bookRow").on("click", function() {
+        console.log("LOG " + this.id);
+    })
+
+
+    gather = function() {
+        if (window.localStorage.getItem("tabler")) {
+            data = window.localStorage.getItem("tabler");
+            $("#loader").hide();
+            bTable = "<tr><th>Title</th><th>Author</th><th>Date</th></tr>"
+            $("#bookTable").html(bTable + data);
+        } else {
+            $.ajax({
+                url: "http://seeward.com/books_app.php",
+                cache: true,
+                dataType: "json",
+            }).done(function(html) {
+                $("#loader").hide();
+
+                renderTable(html);
+
+            });
+        }
+    }
+
+
+    $("#bookTable").searcher({
+        inputSelector: "#searchTerm"
+        // itemSelector (tbody > tr) and textSelector (td) already have proper default values
+    });
 
 
 
@@ -37,7 +65,7 @@ $("#bookTable").searcher({
 
 
 
-
+    init();
 
 
 
